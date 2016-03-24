7947369d8386bc006cf5ec87390c1a30f1635f00
x19990416 <starseeker.limin@gmail.com>
2016-03-24 09:39:15 +0800

    1

--- src/test/java/a.java
+++ src/test/java/a.java
@@ -1,11 +1,12 @@
-import com.macrossx.tuple.TupleUtil;
+import com.macrossx.common.tuple.TupleUtil;
 
 
 public class a {
 
 	public static void main(String[] args) {
-		System.out.println(TupleUtil.of(0.0000000000000004d, 1, null, 0.0000000000000004d));
+		System.out.println(TupleUtil.of(0.0000000000000004d, 1, null, 0.0000000000000004d)._1);
 
 	}
 
 }
+	
