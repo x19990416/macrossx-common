dac3fc22db0a509b24aa80ad6bb1ca42463bbc95
x19990416 <starseeker.limin@gmail.com>
2016-11-25 14:00:00 +0800

    update

--- src/main/java/com/macrossx/common/tuple/Tuple2.java
+++ src/main/java/com/macrossx/common/tuple/Tuple2.java
@@ -11,16 +11,16 @@ package com.macrossx.common.tuple;
  *            Nullable，元组的第2个元素
  */
 public class Tuple2<T1, T2> extends Tuple1<T1> {
-	public Tuple2(T1 _1, T2 _2) {
-		super(_1);
-		this._2 = _2;
+	public Tuple2(T1 t_1, T2 t_2) {
+		super(t_1);
+		this.t_2 = t_2;
 	}
 
-	public final T2 _2;
+	public final T2 t_2;
 
 	@Override
 	public String toString() {
-		return "(" + _1 + "," + _2 + ")";
+		return "(" + t_1 + "," +t_2 + ")";
 	}
 
 }
